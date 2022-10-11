f8d8bdd237165f468a2559056049f53e7d21f843
Jakub Łata <lootjakub@gmail.com>
2022-10-11 10:52:22 +0200

    added the models and database

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Flashcard(models.Model):
+    front = models.TextField(blank=True)
+    back = models.TextField(blank=True)
+    learned = models.BooleanField(default=False)
+
+def serialize(self):
+    return {
+        "id": self.id,
+        "front": self.front,
+        "back": self.back,
+        "learned": self.learned
+    }
